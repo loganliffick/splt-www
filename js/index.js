efcfdad977fb911eb6d1c0c939e22e0fb2bd729e
Logan Liffick <ltliffick@gmail.com>
2021-04-14 16:56:58 -0400

    hello-world

---
+++ js/index.js
@@ -0,0 +1,17 @@
+import splt from "spltjs";
+import anime from "animejs";
+
+splt({
+  reveal: true,
+});
+
+anime({
+  targets: ".reveal",
+  translateY: [40, 0],
+  duration: 1000,
+  loop: true,
+  direction: "alternate",
+  delay: anime.stagger(25, { start: 500 }),
+  endDelay: 500,
+  easing: "cubicBezier(.71,-0.77,.43,1.67)",
+});
